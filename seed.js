9b32ee5b5201c4e7ca3e0f3289470c9b1e34f5c6
Gabriella Lubliner <gabriella.lubliner@gmail.com>
2015-11-05 16:35:09 -0500

    began seeding database

---
+++ seed.js
@@ -0,0 +1,4 @@
+db.likeHome.insert(
+	{ name: "Crazy Thanksgiving Party", date: "26 - Nov - 2015", location: 10022, interests: [ "entertainment", "win appreciation" ], others: "N/A", holiday: "Thanksgiving", desc: "Turkey, whisky, and good company!", host: "Lindsay", guests: [ "Tom", "Jerry", "Matt", "Mark", "Luke", "John", "Vince", "E", "Turtle", "Drama" ] }
+
+	)
